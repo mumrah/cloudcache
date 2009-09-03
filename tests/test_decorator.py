f11422c338070c9b212c82d83f46f8e501f8e8a7
David Arthur <mumrah@gmail.com>
2009-09-03 09:33:30 -0400

    last commit, i swear

--- tests/test_decorator.py
+++ tests/test_decorator.py
@@ -1,12 +1,15 @@
 from cloudcache import cached
 import nose
 from ConfigParser import ConfigParser
-
 config = ConfigParser()
 config.read('my.conf')
 
 cached.aws_access_key_id = config.get('aws','access_key_id')
 cached.aws_secret_access_key = config.get('aws','secret_access_key')
+
+#
+# Yea, so this doesn't really test anything. My bad.
+#
 
 @cached(expires=60)
 def testing(a,b,c):
